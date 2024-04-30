f384417c27e11a87f6873fbe2e82736149d8636d
Siya <siajabbarova@gmail.com>
2024-04-30 22:47:50 +0400

    first

---
+++ Validation-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Validation_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
