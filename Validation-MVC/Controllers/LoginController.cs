70d11dd736bdee9734f69deedbd82f38256b0f09
Siya <siajabbarova@gmail.com>
2024-04-30 22:47:50 +0400

    Update Views, Fluent Validator ,Login

---
+++ Validation-MVC/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Validation_MVC.Models;
+using Validation_MVC.Services;
+
+namespace Validation_MVC.Controllers;
+
+public class LoginController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+   
+    [HttpPost]
+    public IActionResult Index(User user)
+    {
+        if (!ModelState.IsValid)
+        {
+            user.Id = Guid.NewGuid();
+            DbContext.Users.Add(user);
+            return View();
+        }
+        return RedirectToAction("Index", "Home");
+    }
+    
+    
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Login(User user)
+    {
+        var logUser = DbContext.Users.FirstOrDefault(u => u.Username == user.Username);
+        if (logUser is not null)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        return View();
+    }
+}
